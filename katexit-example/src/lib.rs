341e5894aae1b6f311db2733fd7894c9baa8dfbc
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-08-28 16:03:44 +0900

    Add display style example

--- katexit-example/src/lib.rs
+++ katexit-example/src/lib.rs
@@ -2,4 +2,11 @@ use katexit::katexit;
 
 #[katexit]
 /// We can write $\LaTeX$ expressions
+///
+/// Display style
+/// -------------
+///
+/// $$
+/// c = \\pm\\sqrt{a^2 + b^2}
+/// $$
 pub fn my_func() {}
