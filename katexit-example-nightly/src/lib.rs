160a47c751837e05b015fb97a080914ab521d767
Toshiki Teramura <toshiki.teramura@gmail.com>
2022-10-29 21:28:00 +0900

    Add document for https://github.com/rust-lang/rust/issues/54726

--- katexit-example-nightly/src/lib.rs
+++ katexit-example-nightly/src/lib.rs
@@ -1,6 +1,9 @@
 #![feature(prelude_import, custom_inner_attributes)]
 #![cfg_attr(doc, katexit::katexit)]
 //! Module level document example with $\KaTeX$!
+//!
+//! This requires nightly rustc for using [custom_inner_attributes](https://github.com/rust-lang/rust/issues/54726)
+//!
 
 #[cfg_attr(doc, katexit::katexit)]
 /// We can write $\LaTeX$ expressions
